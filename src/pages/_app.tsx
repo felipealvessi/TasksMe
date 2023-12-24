2805b1f287895d541c8bd5b4723f9901d69dcd06
Felipe Alves <felipealvesinfor@gmail.com>
2023-12-23 22:29:40 -0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import { SessionProvider } from "next-auth/react"
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Header } from './components/header'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <SessionProvider session={pageProps.session}>
+    <Header />
+    <Component {...pageProps} />
+  </SessionProvider>
+  );
 }
