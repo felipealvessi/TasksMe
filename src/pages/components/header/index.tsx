8bbd774330dc5787ab153757a3424af96e00710e
Felipe Alves <felipealvesinfor@gmail.com>
2023-12-23 21:32:35 -0300

    Creating the components

---
+++ src/pages/components/header/index.tsx
@@ -0,0 +1,45 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+import styles from './styles.module.css'
+
+import Link from 'next/link';
+
+
+export function Header() {
+
+  const { data: session, status } = useSession();
+
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+        <Link href="/">
+          <h1 className={styles.logo}>
+            Tasks
+              <span>
+              Me
+              </span>
+           </h1>
+        </Link>
+          {session?.user && (
+          <Link href="/dashboard" className={styles.link}>
+             Meu Painel
+           </Link>
+          )}
+        </nav>
+
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button className={styles.loginButton} onClick={() => signOut()}>
+            Olá, {session?.user?.name} 😁
+          </button>
+        ) : (	
+          <button className={styles.loginButton} onClick={() => signIn("google")}>
+            Acessar
+          </button>
+        )
+        }
+      </section>
+    </header>
+  );
+}
