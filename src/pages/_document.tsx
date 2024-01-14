1155534f1c51ae49569d70304b196621f02bb6cb
Felipe Alves <felipealvesinfor@gmail.com>
2024-01-14 04:10:52 -0300

    Add Vercel Speed Insights Quickstart

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function Document() {
   return (
     <Html lang="pt-BR">
@@ -7,6 +9,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
   )
