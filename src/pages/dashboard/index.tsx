6930fe04b0d8ff4f87ba560a3d23189516137ceb
Felipe Alves <felipealvesinfor@gmail.com>
2024-03-20 23:00:37 -0300

    adjusting the style and functionality of the admin panel

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,54 @@
 import { GetServerSideProps } from "next";
-
+import { ChangeEvent, FormEvent, useState } from "react";
 import styles from "./styles.module.css";
 import Head from "next/head";
 
 import { getSession } from "next-auth/react";
 import { Textarea } from "../components/textarea";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 
+import { db } from "../../services/firebaseConnection";
 
-export default function Dashboard() {
+import { addDoc, collection } from 'firebase/firestore';
+
+interface HomeProps {
+  user: {
+    email: string;
+  };
+}
+
+export default function Dashboard({ user }: HomeProps) {
+  const [input, setInput] = useState("");
+  const [publicTask, setPublicTask] = useState(false);
+
+
+  function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
+    setPublicTask(event.target.checked)
+  }
+
+  async function handleRegisterTask(event: FormEvent) {
+    event.preventDefault();
+
+    if (input === "") return;
+
+    try {
+      await addDoc(collection(db, "tasksme"), {
+        task: input,
+        created: new Date(),
+        user: user?.email,
+        public: publicTask,
+      });
+
+      setInput("");
+      setPublicTask(false);
+    } catch (err) {
+      console.log(err);
+
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,15 +59,42 @@ export default function Dashboard() {
         <section className={styles.content}>
           <div className={styles.contentForm}>
             <h1 className={styles.title}>Qual sua tarefa? </h1>
-              <form>
-                <Textarea placeholder='Digite sua tarefa...'/>
-                <div className={styles.checkboxArea}>
-                <input type="checkbox" placeholder="checkbox" className={styles.checkbox}/>
+            <form onSubmit={handleRegisterTask}>
+              <Textarea placeholder='Digite sua tarefa...'
+                value={input}
+                onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setInput(event.target.value)}
+              />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" placeholder="checkbox" className={styles.checkbox}
+                  checked={publicTask}
+                  onChange={handleChangePublic}
+                />
                 <label>Deixar tarefa publica?</label>
-                </div>
-                <button className={styles.button} type="submit">Registrar</button>
-              </form>
+              </div>
+              <button className={styles.button} type="submit">Registrar</button>
+            </form>
           </div>
+        </section>
+
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas Tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>Minha Primeira tarefa de exemplo show demais!</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140" />
+              </button>
+            </div>
+          </article>
         </section>
       </main>
     </div>
@@ -38,7 +105,7 @@ export default function Dashboard() {
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
 
-  if (!session) {
+  if (!session?.user) {
     return {
       redirect: {
         destination: "/",
@@ -48,6 +115,10 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   }
 
   return {
-    props: {},
+    props: {
+      user: {
+        email: session?.user?.email,
+      },
+    },
   };
 };
