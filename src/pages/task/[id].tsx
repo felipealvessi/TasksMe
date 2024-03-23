b76d20c1ca7f63bc706f43c2798e9b494c6fcf13
Felipe Alves <felipealvesinfor@gmail.com>
2024-03-22 23:44:49 -0300

    adding task page

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,15 +1,88 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import styles from "./styles.module.css";
 
-export default function Task() {
+import { db } from "../../services/firebaseConnection";
+import { doc, collection, query, where, getDoc } from "firebase/firestore";
+
+import { Textarea } from "../components/textarea";
+
+interface TaskProps {
+  item: {
+    tasks: string;
+    created: string;
+    public: boolean;
+    user: string;
+    taskId: string;
+  };
+}
+
+export default function Task({ item }: TaskProps) {
   return (
-    <div >
+    <div className={styles.container}>
       <Head>
-        <title>Detalhes da Tarefa</title>
+        <title>Tarefa - Detalhes da tarefa</title>
       </Head>
 
-      <main>
+      <main className={styles.main}>
         <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>{item.tasks}</p>
+        </article>
       </main>
+
+      <section className={styles.commentsContainer}>
+        <h2>Deixar comentário</h2>
+
+        <form>
+          <Textarea placeholder="Digite seu comentário..." />
+          <button className={styles.button}>Enviar comentário</button>
+        </form>
+      </section>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const id = params?.id as string;
+
+  const docRef = doc(db, "tasks", id);
+
+  const snapshot = await getDoc(docRef);
+
+  if (snapshot.data() === undefined) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  if (!snapshot.data()?.public) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const miliseconds = snapshot.data()?.created?.seconds * 1000;
+
+  const task = {
+    tasks: snapshot.data()?.tasks,
+    public: snapshot.data()?.public,
+    created: new Date(miliseconds).toLocaleDateString(),
+    user: snapshot.data()?.user,
+    taskId: id,
+  };
+
+  console.log(task);
+
+  return {
+    props: {
+      item: task,
+    },
+  };
+};
